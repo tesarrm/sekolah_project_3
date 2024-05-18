bdb8eaeb4857d446da418c3cd9e682c24bcd766a
Tesar Rahmat Maulana <tesarrm58@gmail.com>
2024-05-18 14:30:16 +0700

    kelolah data hanya yang berelasi dengan sekolah yang sama

--- user/urls.py
+++ user/urls.py
@@ -5,24 +5,25 @@ from .views import (
     StaffSekolahViewSet, SiswaViewSet, OrangTuaViewSet,
     LoginView, UserDetailView,
     LogoutView,
+    AnakViewSet
 )
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
+# router.register(r'users', UserViewSet)
 # router.register(r'superadmins', SuperAdminViewSet)
-router.register(r'adminsekolah', AdminSekolahViewSet)
-router.register(r'staffsekolah', StaffSekolahViewSet)
-router.register(r'siswa', SiswaViewSet)
-router.register(r'orangtua', OrangTuaViewSet)
+# router.register(r'adminsekolah', AdminSekolahViewSet)
+# router.register(r'staffsekolah', StaffSekolahViewSet)
+# router.register(r'siswa', SiswaViewSet)
+# router.register(r'orangtua', OrangTuaViewSet)
+router.register(r'adminsekolah', AdminSekolahViewSet, basename='adminsekolah')
+router.register(r'staffsekolah', StaffSekolahViewSet, basename='staffsekolah')
+router.register(r'siswa', SiswaViewSet, basename='siswa')
+router.register(r'orangtua', OrangTuaViewSet, basename='orangtua')
 
 urlpatterns = [
+    path('anak/', AnakViewSet.as_view(), name='anak-list'),
     path('', include(router.urls)),
     path('login/', LoginView.as_view(), name='login'),
     path('me/', UserDetailView.as_view(), name='me'),
-    # path('register/superadmin/', SuperAdminRegisterView.as_view(), name='register-superadmin'),
-    # path('register/adminsekolah/', AdminSekolahRegisterView.as_view(), name='register-adminsekolah'),
-    # path('register/staffsekolah/', StaffSekolahRegisterView.as_view(), name='register-staffsekolah'),
-    # path('register/siswa/', SiswaRegisterView.as_view(), name='register-siswa'),
-    # path('register/orangtua/', OrangTuaRegisterView.as_view(), name='register-orangtua'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -32,25 +32,94 @@ class UserViewSet(viewsets.ModelViewSet):
 #     permission_classes = [IsAuthenticated, IsSuperAdminOrReadOnly]
 
 class AdminSekolahViewSet(viewsets.ModelViewSet):
-    queryset = AdminSekolah.objects.all()
     serializer_class = AdminSekolahSerializer
     permission_classes = [IsAuthenticated, IsSuperAdminOrReadOnly]
 
+    def get_queryset(self):
+        user = self.request.user
+        if hasattr(user, 'superadmin'):
+            return AdminSekolah.objects.all()
+        elif hasattr(user, 'adminsekolah'):
+            return AdminSekolah.objects.filter(sekolah=user.adminsekolah.sekolah)
+        elif hasattr(user, 'staffsekolah'):
+            return AdminSekolah.objects.filter(sekolah=user.staffsekolah.sekolah)
+        elif hasattr(user, 'siswa'):
+            return AdminSekolah.objects.filter(sekolah=user.siswa.sekolah)
+        elif hasattr(user, 'orangtua'):
+            return OrangTua.objects.filter(siswa__sekolah=user.orangtua.siswa.sekolah)
+        else:
+            return AdminSekolah.objects.none()
+
 class StaffSekolahViewSet(viewsets.ModelViewSet):
-    queryset = StaffSekolah.objects.all()
     serializer_class = StaffSekolahSerializer
     permission_classes = [IsAuthenticated, IsSuperAdminAndAdminSekolahOrReadOnly]
 
+    def get_queryset(self):
+        user = self.request.user
+        if hasattr(user, 'superadmin'):
+            return StaffSekolah.objects.all()
+        elif hasattr(user, 'adminsekolah'):
+            return StaffSekolah.objects.filter(sekolah=user.adminsekolah.sekolah)
+        elif hasattr(user, 'staffsekolah'):
+            return StaffSekolah.objects.filter(sekolah=user.staffsekolah.sekolah)
+        elif hasattr(user, 'siswa'):
+            return StaffSekolah.objects.filter(sekolah=user.siswa.sekolah)
+        elif hasattr(user, 'orangtua'):
+            return OrangTua.objects.filter(siswa__sekolah=user.orangtua.siswa.sekolah)
+        else:
+            return StaffSekolah.objects.none()
+
 class SiswaViewSet(viewsets.ModelViewSet):
-    queryset = Siswa.objects.all()
     serializer_class = SiswaSerializer
     permission_classes = [IsAuthenticated, IsSuperAdminAndAdminSekolahAndStaffSekolahOrReadOnly]
 
+    def get_queryset(self):
+        user = self.request.user
+        if hasattr(user, 'superadmin'):
+            return Siswa.objects.all()
+        elif hasattr(user, 'adminsekolah'):
+            return Siswa.objects.filter(sekolah=user.adminsekolah.sekolah)
+        elif hasattr(user, 'staffsekolah'):
+            return Siswa.objects.filter(sekolah=user.staffsekolah.sekolah)
+        elif hasattr(user, 'siswa'):
+            return Siswa.objects.filter(sekolah=user.siswa.sekolah)
+        elif hasattr(user, 'orangtua'):
+            return OrangTua.objects.filter(siswa__sekolah=user.orangtua.siswa.sekolah)
+        else:
+            return Siswa.objects.none()
+
 class OrangTuaViewSet(viewsets.ModelViewSet):
-    queryset = OrangTua.objects.all()
     serializer_class = OrangTuaSerializer
     permission_classes = [IsAuthenticated, IsSuperAdminAndAdminSekolahAndStaffSekolahOrReadOnly]
 
+    def get_queryset(self):
+        user = self.request.user
+        if hasattr(user, 'superadmin'):
+            return OrangTua.objects.all()
+        elif hasattr(user, 'adminsekolah'):
+            return OrangTua.objects.filter(siswa__sekolah=user.adminsekolah.sekolah)
+        elif hasattr(user, 'staffsekolah'):
+            return OrangTua.objects.filter(siswa__sekolah=user.staffsekolah.sekolah)
+        elif hasattr(user, 'siswa'):
+            return OrangTua.objects.filter(siswa__sekolah=user.siswa.sekolah)
+        elif hasattr(user, 'orangtua'):
+            return OrangTua.objects.filter(siswa__sekolah=user.orangtua.siswa.sekolah)
+        else:
+            return OrangTua.objects.none()
+
+class AnakViewSet(APIView):
+    def get(self, request):
+        user = request.user
+        try:
+            # Ambil orang tua berdasarkan user yang terautentikasi
+            orang_tua = OrangTua.objects.get(user=user)
+            # Ambil semua siswa yang terkait dengan orang tua tersebut
+            anak_anak = orang_tua.siswa.all()
+            # Serialize data siswa
+            serializer = SiswaSerializer(anak_anak, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except OrangTua.DoesNotExist:
+            return Response({"message": "Orang tua tidak ditemukan"}, status=status.HTTP_404_NOT_FOUND)
 
 # Authentication
 
@@ -64,7 +133,6 @@ class LoginView(views.APIView):
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         return Response({'token': token.key}, status=status.HTTP_200_OK)
-
 
 ## User detail current login
 
@@ -87,30 +155,6 @@ class UserDetailView(APIView):
             return Response({'detail': 'User type not recognized'}, status=400)
 
         return Response(serializer.data)
-
-
-## Register
-
-# class SuperAdminRegisterView(generics.CreateAPIView):
-#     queryset = SuperAdmin.objects.all()
-#     serializer_class = SuperAdminRegisterSerializer
-
-# class AdminSekolahRegisterView(generics.CreateAPIView):
-#     queryset = AdminSekolah.objects.all()
-#     serializer_class = AdminSekolahRegisterSerializer
-
-# class StaffSekolahRegisterView(generics.CreateAPIView):
-#     queryset = StaffSekolah.objects.all()
-#     serializer_class = StaffSekolahRegisterSerializer
-
-# class SiswaRegisterView(generics.CreateAPIView):
-#     queryset = Siswa.objects.all()
-#     serializer_class = SiswaRegisterSerializer
-
-# class OrangTuaRegisterView(generics.CreateAPIView):
-#     queryset = OrangTua.objects.all()
-#     serializer_class = OrangTuaRegisterSerializer
-
 
 ## Logout
 
